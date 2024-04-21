ba2702ade2400fe9b12576d743c3ec42b692d470
San <sganis@gmail.com>
2024-04-21 10:39:33 +0300

    Update build.bat

--- build.bat
+++ build.bat
@@ -84,7 +84,7 @@ if exist openssl-%OPENSSL% rd /s /q openssl-%OPENSSL%
 cd openssl-%OPENSSL%
 
 perl Configure                ^
-	no-shared no-stdio no-sock no-engine no-comp no-ssl3	^
+	no-shared no-stdio no-sock no-comp no-ssl3 ^
 	VC-%OARCH%            ^
 	--prefix=%PREFIX%     ^
 	--openssldir=%PREFIX%
